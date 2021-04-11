11e99e403d41cb6579b70bc015d200e0df7119c1
Becky Tseng <becky.tseng@titansoft.com.sg>
2021-04-11 18:45:23 +0800

    add BandNameGenerator

---
+++ C#/Kata/BandNameGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kata
+{
+    public class BandNameGenerator
+    {
+        public static string GenerateBandName(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return "";
+
+            if (str[0] == str[str.Length - 1])
+            {
+                str = Char.ToUpper(str[0]) + str.Substring(1) + str.Substring(1);
+            }
+            else
+            {
+                str = "The " + Char.ToUpper(str[0]) + str.Substring(1);
+            }
+
+            return str;
+        }
+    }
+}
--- C#/Kata/Program.cs
+++ C#/Kata/Program.cs
@@ -7,8 +7,11 @@ namespace Kata
         static void Main(string[] args)
         {
             //Console.WriteLine(StringAverage.AverageString("two three"));
-            Console.WriteLine(DeadAnts.DeadAntCount("...ant...ant..nat.ant.t..ant...ant..ant..ant.an ant..t")); // output 2
+
+            //Console.WriteLine(DeadAnts.DeadAntCount("...ant...ant..nat.ant.t..ant...ant..ant..ant.an ant..t")); // output 2
             //Console.WriteLine(DeadAnts.DeadAntCount(null)); // output 0
+
+            Console.WriteLine(BandNameGenerator.GenerateBandName("test"));
         }
 
     }
