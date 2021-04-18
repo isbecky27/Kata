9007a5e6e163647222f1ced97d8becd9c374f810
Becky Tseng <becky.tseng@titansoft.com.sg>
2021-04-18 11:59:40 +0800

    add NextBiggerNumberWithTheSameDigits

---
+++ C#/Kata/NextBiggerNumberWithTheSameDigits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+    class NextBiggerNumberWithTheSameDigits
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            var numberList = n.ToString().ToList();
+
+            for (var i = numberList.Count - 1; i > 0; i--)
+            {
+                if (numberList[i] > numberList[i - 1])
+                {
+                    var index = i;
+                    for (var j = numberList.Count - 1; j >= i; j--)
+                    {
+                        if (numberList[j] > numberList[i - 1])
+                        {
+                            index = j;
+                            break;
+                        }
+                    }
+
+                    var temp = numberList[i - 1];
+                    numberList[i - 1] = numberList[index];
+                    numberList[index] = temp;
+                    numberList.Sort(i, numberList.Count - i, null);
+
+                    return Convert.ToInt64(string.Join("", numberList));
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- C#/Kata/Program.cs
+++ C#/Kata/Program.cs
@@ -7,12 +7,16 @@ namespace Kata
         static void Main(string[] args)
         {
             //Console.WriteLine(StringAverage.AverageString("two three"));
-
             //Console.WriteLine(DeadAnts.DeadAntCount("...ant...ant..nat.ant.t..ant...ant..ant..ant.an ant..t")); // output 2
             //Console.WriteLine(DeadAnts.DeadAntCount(null)); // output 0
-
             //Console.WriteLine(BandNameGenerator.GenerateBandName("test"));
-            Console.WriteLine(CountingDuplicates.DuplicateCount("aabbcde"));
+            //Console.WriteLine(CountingDuplicates.DuplicateCount("aabbcde"));
+            while (true)
+            {
+                var i = Console.ReadLine();
+                Console.WriteLine(NextBiggerNumberWithTheSameDigits.NextBiggerNumber(Convert.ToInt64(i)));
+            }
+            
         }
 
     }
