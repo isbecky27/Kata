0f5c014f77143f6df50c522b3294ad6050e6916a
Becky Tseng <becky.tseng@titansoft.com.sg>
2021-04-18 12:06:23 +0800

    add ROT13

---
+++ C#/Kata/ROT13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+    public class ROT13
+    {
+        public static string Rot13(string input)
+        {
+            var output = input.ToList();
+
+            for (var i = 0; i < output.Count; i++)
+            {
+                if ((output[i] >= 'A' && output[i] <= 'M') || (output[i] >= 'a' && output[i] <= 'm'))
+                {
+                    output[i] = (char)(Convert.ToInt32(output[i]) + 13);
+                }
+                else if ((output[i] >= 'N' && output[i] <= 'Z') || (output[i] >= 'n' && output[i] <= 'z'))
+                {
+                    output[i] = (char)(Convert.ToInt32(output[i]) - 13);
+                }
+            }
+
+            return String.Join("", output);
+        }
+    }
+}
